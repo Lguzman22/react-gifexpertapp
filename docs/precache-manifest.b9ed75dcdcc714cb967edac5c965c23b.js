134daba98d3a6dfd99318bb0049a4eb1c5c4aaef
Luis Guzman <lguzman@baufest.com>
2020-09-29 14:53:29 -0300

    Docs

---
+++ docs/precache-manifest.b9ed75dcdcc714cb967edac5c965c23b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2be7fac7d3bf58984fe70b729f91484c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66de0517df4fc8c28cf0",
+    "url": "/static/css/main.e4d8ea94.chunk.css"
+  },
+  {
+    "revision": "26227e71ad7d4f1513b6",
+    "url": "/static/js/2.d7cf9fae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d7cf9fae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66de0517df4fc8c28cf0",
+    "url": "/static/js/main.51c227d3.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
